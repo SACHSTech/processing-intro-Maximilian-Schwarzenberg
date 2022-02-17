413e8604ade7dea9cd0e17f122017fbac2982aca
Maximilian Schwarzenberg <78431850+Maximilian-Schwarzenberg@users.noreply.github.com>
2022-02-17 20:49:05 +0000

    Drew the house

--- Sketch.java
+++ Sketch.java
@@ -12,11 +12,11 @@ public class Sketch extends PApplet {
   }
 
   /** 
-   * Called once at the beginning of execution.  Add initial set up
+   * Called once at the beginning of execution.  Add initial setup
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(135, 206, 235);
   }
 
   /**
@@ -25,11 +25,44 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    // Green rectangle for grass
+    fill(86, 125, 70);
+    rect(0, 300, 400, 100);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    
+    // Base square for house
+    fill(255, 248, 220);
+    rect(100, 100, 200, 200);
+
+    // Triangle for Roof
+    fill(170, 1, 20);
+    triangle(100, 100, 300, 100, 200, 25);
+
+    // Rectangle for door
+    fill(196, 164, 132);
+    rect(175, 225, 50, 75);
+
+    // Squares for windows
+    fill(22, 184, 243);
+    
+    rect(120, 140, 60, 60);
+    rect(220, 140, 60, 60);
+
+    // Yellow circle for sun
+    fill(255, 233, 0);
+    ellipse(50, 50, 50, 50);
+
+    // Window design
+    line(150, 140, 150, 200);
+    line(120, 170, 180, 170);
+
+    line(250, 140, 250, 200);
+    line(220, 170, 280, 170);
+
+    // Door design
+    fill(0, 0, 0);
+    ellipse(220, 260, 5, 5);
+    
   }
   
   // define other methods down here.
