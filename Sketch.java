e3454c9c96e1da4ef17d7f2ad446b99564a54c4e
Maximilian Schwarzenberg <78431850+Maximilian-Schwarzenberg@users.noreply.github.com>
2022-02-22 16:33:29 +0000

    Updated comments

--- Sketch.java
+++ Sketch.java
@@ -16,7 +16,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(135, 206, 235);
+    background(135, 206, 235); // Light blue
   }
 
   /**
@@ -25,31 +25,30 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    // Green rectangle for grass
-    fill(86, 125, 70);
+    // Rectangle for grass
+    fill(86, 125, 70); //Green
     rect(0, 300, 400, 100);
-
     
     // Base square for house
-    fill(255, 248, 220);
+    fill(255, 248, 220); // White
     rect(100, 100, 200, 200);
 
     // Triangle for Roof
-    fill(170, 1, 20);
+    fill(170, 1, 20); // Red
     triangle(100, 100, 300, 100, 200, 25);
 
     // Rectangle for door
-    fill(196, 164, 132);
+    fill(196, 164, 132); // Light brown
     rect(175, 225, 50, 75);
 
     // Squares for windows
-    fill(22, 184, 243);
+    fill(22, 184, 243); // Blue
     
     rect(120, 140, 60, 60);
     rect(220, 140, 60, 60);
 
-    // Yellow circle for sun
-    fill(255, 233, 0);
+    // Circle for sun
+    fill(255, 233, 0); //Yellow
     ellipse(50, 50, 50, 50);
 
     // Window design
@@ -60,7 +59,7 @@ public class Sketch extends PApplet {
     line(220, 170, 280, 170);
 
     // Door design
-    fill(0, 0, 0);
+    fill(0, 0, 0); // Black
     ellipse(220, 260, 5, 5);
     
   }
